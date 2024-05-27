3597e3f34454e832490ee087c18b46d8dda8a759
Niel2904 <nielmar1234@gmail.com>
2024-05-27 15:38:21 +0800

    Making View - Nel

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FacilitiesController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/faci', [FacilitiesController::class, 'wow']);
