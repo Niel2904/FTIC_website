b0d8153d61c11137dc82a0eccd6287c913dd2734
Joeljr <joelcatapang03.jc@gmail.com>
2024-06-04 10:04:31 +0800

    -joel

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FacilitiesController;
 use App\Http\Controllers\LandingController;
 use App\Http\Controllers\ResearchController;
+use App\Http\Controllers\ContactUsController;
 
 
 Route::get('/', [LandingController::class, 'land']);
 Route::get('/facilities', [FacilitiesController::class, 'wow']);
 Route::get('/research', [ResearchController::class, 'restab']);
+Route::get('/contactus', [ContactUsController::class, 'contacttab']);
 
