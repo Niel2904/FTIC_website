443fb1399cf6f65a7b1e3472ec72ff6c53fdf8d7
Mj <dilig.mj@gmail.com>
2024-06-20 09:32:59 +0800

    -mj

--- resources/views/ProgramView.blade.php
+++ resources/views/ProgramView.blade.php
@@ -30,7 +30,7 @@
                 <div class="single drop">
                     <div class="image"><img src="{{ asset('assets\css\imgs\ARISE-Logo-DARK.png') }}" width="100%"></div>
                     <div class="image-descrip">
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                        <p>UNMASKED gives a sneak peek into the lives of successful entrepreneurs and startups so that they may inspire students to break the mold and create their own startups.</p>
                     </div>
                 </div>
                 
